cf4e8ed8c7631e16720a11add8edca75c0282007
Filipe Alves <filipealves70@gmail.com>
2024-01-29 17:33:37 -0300

    first commit

---
+++ servidor/src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import routeProduto from "./routes/route.produto.js";
+import routePedido from "./routes/route.pedido.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// rotas
+app.use(routeProduto);
+app.use(routePedido);
+
+
+
+app.listen(3001, function(){
+    console.log("Servidor executando na porta 3001");
+});
